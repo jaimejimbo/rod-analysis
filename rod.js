3653f9f1d8c2e6fc8a7cb2c06c64f7c032664f39
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-10-14 14:23:03 +0200

    rod: 	- Creator

--- rod.js
+++ rod.js
@@ -4,7 +4,21 @@
  *  license: GPL
  */ 
 
-var rod = function(){
-
+var rod = function(ID, area, xm, ym, major, minor, angle, feret, feretx, ferety, feretangle, minferet, xstart, ystart){
+    this.ID = ID;
+    this.area = area;
+    this.xm = xm;
+    this.ym = ym;
+    this.major = major;
+    this.minor = minor;
+    this.angle = angle;
+    this.feret = feret;
+    this.feretx = feretx;
+    this.ferety = ferety;
+    this.feretangle = feretangle;
+    this.minferet = minferet;
+    this.xstart = xstart;
+    this.ystart = ystart;
+    this.LD = this.major/this.minor;
 };
 
