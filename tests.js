bc8df77b75b1236698226bf76cfcfe3ef1e6b1a3
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-10-14 14:51:03 +0200

    index: 	- Include ajax Tests: 	- Trying to import files from desktop

--- tests.js
+++ tests.js
@@ -3,6 +3,19 @@
  *  email: 19.jaime.91@gmail.com
  *  license: GPL
  */ 
-
+//http://stackoverflow.com/questions/18480550/how-to-load-all-the-images-from-one-of-my-folder-into-my-web-page-using-jquery
+var dir = "./";
+var fileextension = ".txt";
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+        //List all .png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http://", "");
+            $("body").append("<img src='" + dir + filename + "'>");
+        });
+    }
+});
 
 
