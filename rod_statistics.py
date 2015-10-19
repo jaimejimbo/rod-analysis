56033c9afa9dfa1e7cac4b633708d5ac3e1533e1
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-10-19 20:34:42 +0200

    - Outline of main methods

--- rod_statistics.py
+++ rod_statistics.py
@@ -1,19 +1,50 @@
 """
 Analyze rods data
 """
+from queue import Queue
 
 class Rod(object):
 	"""
 	Rod object.
 	"""
-	pass
+	
+	def __init__(self, ARGS):
+		"""
+		Initialization of rod
+		"""
+	    pass
+
+    def check_if_rod(self):
+        """
+        Checks if this is a rod looking at different factors
+        """
+        return True
+	
+
 
 class RodGroup(object):
 	"""
 	Group of rods.
 	Each image has to be translated into a RodGroup (by this class?)
 	"""
-	pass
+	def __init__(self):
+        """
+        Initialization
+        """
+	    self._rods = Queue()    #I use a Queue, but perhaps is better a tree or a recursive list
+
+    def add_rod(self, rod):
+        """
+        Adds a rod to the group
+        """
+        self._rods.join(rod)
+
+    def remove_rod(self, rod):
+        """
+        Removes a rod from the group (queue object mod needed)
+        """
+        pass
+	
 
 def import_files(folder, ext):
 	"""
