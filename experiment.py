ad3f0b187e90415b8469ae6b4e9348c31de06d5c
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-11-08 14:16:30 +0100

    Errors with multiprocessing. Shared variables seem not to change after running the process.

--- experiment.py
+++ experiment.py
@@ -28,12 +28,14 @@ class Experiment(object):
             raise TypeError;
         self._diff_t = diff_t
         self._evolution_dictionaries = []
+        self._conflictive_final_rods = []
 
     def _reset(self):
         """
         Resets all variables, so they must be computed again.
         """
         self._evolution_dictionaries = []
+        self._conflictive_final_rods = []
 
     def _create_evolution_dict_keys(self):
         """
@@ -42,6 +44,7 @@ class Experiment(object):
         """
         for dummy_index in range(len(self._states)-1):
             self._evolution_dictionaries.append({})
+            self._conflictive_final_rods.append(set([]))
         for index in range(len(self._states)-1):
             state = self._states[index]
             evol_dict = self._evolution_dictionaries[index]
@@ -52,18 +55,27 @@ class Experiment(object):
         """
         Looks for rods that have only one possible predecessor.
         """
+        processes = []
         for index in range(len(self._states)-1):
-            initial_state = self._states[index]
-            final_state = self._states[index+1]
-            evol_dict = self._evolution_dictionaries[index]
-            for initial_rod in initial_state:
-                for final_rod in final_state:
-                    distance = initial_rod.distance_to_rod(final_rod)
-                    angle = abs(initial_rod.angle_between_rods(final_rod))
-                    angle = min(angle, abs(180-angle))
-                    speed = float(distance)/self._diff_t
-                    if speed <= max_speed and angle <= max_angle_diff:
-                        evol_dict[initial_rod.identifier] |= set([final_rod.identifier])
+            processes.append(mp.Process(target=self._fill_evolution_dicts_process,
+                                        args=(index, max_speed, max_angle_diff)))
+        run_processes(processes)
+
+    def _fill_evolution_dicts_process(self, index, max_speed, max_angle_diff):
+        """
+        Allows to create a process and use all cores.
+        """
+        initial_state = self._states[index]
+        final_state = self._states[index+1]
+        evol_dict = self._evolution_dictionaries[index]
+        for initial_rod in initial_state:
+            for final_rod in final_state:
+                distance = initial_rod.distance_to_rod(final_rod)
+                angle = abs(initial_rod.angle_between_rods(final_rod))
+                angle = min(angle, abs(180-angle))
+                speed = float(distance)/self._diff_t
+                if speed <= max_speed and angle <= max_angle_diff:
+                    evol_dict[initial_rod.identifier] |= set([final_rod.identifier])
 
     def _clear_evolution_dicts(self):
         """
@@ -71,12 +83,46 @@ class Experiment(object):
         delete that possible evolution from the rest of rods.
         """
         changed = False
+        changes_queue = mp.Queue()
+        processes = []
         for evol_dict in self._evolution_dictionaries:
-            for initial_rod_id in evol_dict.keys():
-                final_rods = evol_dict[initial_rod_id]
-                if len(final_rods) == 1:
-                    changed = True
-                    _remove_final_rod(evol_dict, initial_rod_id, final_rods)
+            processes.append(mp.Process(target=self._clear_evolution_dicts_process,
+                                        args=(evol_dict, changes_queue)))
+        run_processes(processes)
+        try:
+            element = changes_queue.get()
+            if element:
+                changed = True
+        except:
+            pass
+        return changed
+
+    def _clear_evolution_dicts_process(self, evol_dict, changes_queue):
+        """
+        Process for method.
+        """
+        changed = False
+        for initial_rod_id in evol_dict.keys():
+            final_rods = evol_dict[initial_rod_id]
+            if len(final_rods) == 1:
+                changed = _remove_final_rod(evol_dict, initial_rod_id, final_rods)
+        changes_queue.put(changed)
+
+    def _remove_final_rod(self, evol_dict, initial_rod_id, final_rod):
+        """
+        Remove final rod from the rest of rods' evolutions.
+        """
+        changed = False
+        for rod_id in evol_dict.keys():
+            final = evol_dict[rod_id]
+            counter = 0
+            if rod_id != initial_rod_id:
+                final -= final_rod
+                changed = True
+            if not len(final):
+                final |= final_rod
+                self._conflictive_final_rods |= final_rod
+                changed = False
         return changed
 
     def evolution_dictionaries(self, max_speed, max_angle_diff):
@@ -91,14 +137,33 @@ class Experiment(object):
         if not len(self._evolution_dictionaries):
             self._create_evolution_dict_keys()
             self._fill_evolution_dicts(max_speed, max_angle_diff)
-            self._clear_evolution_dicts()
+            changed = True
+            while changed:
+                changed = self._clear_evolution_dicts()
         return self._evolution_dictionaries            
 
 
-def _remove_final_rod(evol_dict, initial_rod_id, final_rod):
+def run_processes(processes):
     """
-    Remove final rod from the rest of rods' evolutions.
+    Runs all processes using all cores.
     """
-    for rod_id in evol_dict.keys():
-        if rod_id != initial_rod_id:
-            evol_dict[rod_id] -= final_rod
+    cpus = 2*mp.cpu_count()
+    running = []
+    try:
+        for cpu in range(cpus):
+            next_process = processes.pop()
+            running.append(next_process)
+            next_process.start()
+    except IndexError:
+        pass
+    try:
+        for process in running:
+            if not process.is_alive():
+                running.remove(process)
+                next_process = processes.pop()
+                running.append(next_process)
+                next_process.start()
+    except IndexError:
+        pass
+    for process in running:
+        process.join()
