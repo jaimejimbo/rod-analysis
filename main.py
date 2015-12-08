69c88c72fc165bc03dfcf39268b19a51f12cf09a
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-12-08 13:09:45 +0100

    - Report changes - Kill all zombie processes when exiting. - Can't run program for both K5 and K17, because system runs out of memory.

--- main.py
+++ main.py
@@ -18,23 +18,23 @@ try:
     else:
         dates = methods.import_image_dates()
 
-    names, rod_groups_5 = system_state.create_rods(kappas=5.5,
-                                            allowed_kappa_error=.5)
+    #names, rod_groups_5 = system_state.create_rods(kappas=5.5,
+    #                                        allowed_kappa_error=.5)
     names, rod_groups_17 = system_state.create_rods(kappas=17.5,
                                             allowed_kappa_error=1.5)
-    experiment_5 = experiment.Experiment(system_states_name_list=names,
-                                        system_states_list=rod_groups_5,
-                                        dates=dates, diff_t=5/3.0)
+    #experiment_5 = experiment.Experiment(system_states_name_list=names,
+    #                                    system_states_list=rod_groups_5,
+    #                                    dates=dates, diff_t=5/3.0)
     experiment_17 = experiment.Experiment(system_states_name_list=names,
                                         system_states_list=rod_groups_17,
                                         dates=dates, diff_t=5/3.0)
 
-    experiment_5.set_coef(5)
+    #experiment_5.set_coef(5)
     experiment_17.set_coef(5)
     #experiment_5.divide_systems_in_circles(divisions=30)
     #experiment_17.divide_systems_in_circles(divisions=30)
-    experiment_5.create_gifs(divisions=30, fps=10, max_distance=10, max_angle_diff=5,
-                             number_of_bursts=1)
+    #experiment_5.create_gifs(divisions=30, fps=10, max_distance=10, max_angle_diff=5,
+    #                         number_of_bursts=1)
     experiment_17.create_gifs(divisions=30, fps=10, max_distance=10, max_angle_diff=5,
                              number_of_bursts=1)
     #cluster_areas5 = experiment_5.cluster_areas(max_distance=50,
@@ -54,3 +54,5 @@ try:
 except KeyboardInterrupt:
     os.system("sudo pkill -f main.py")
     raise KeyboardInterrupt
+print "Give root password to kill all remaining zombie processes"
+os.system("sudo pkill -f main.py")
