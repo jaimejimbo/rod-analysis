9121aae91eb062b6f35beace657519b0c038a71e
Jaime Perez Aparicio <19.jaime.91@gmail.com>
2016-04-12 21:44:42 +0200

    - Added methods to change rods data files numeration and dates' ids

---
+++ change_rods_name.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#este directorio debe ser el lugar donde estén las fotos que quieres cambiar el nombre
+FILES=rods_*
+i=0
+mkdir carpetaNueva
+
+for f in $FILES
+do
+  echo "Procesando: $f"
+  #no estoy seguro si tienes que poner ".png" o el tipo de foto que tengas al final del $i. Si no funciona, prueba cambiar por: "0_"$i".png" 
+  if (($i<10)); then
+    text="rods_000"$i
+  elif ((i<100)); then
+    text="rods_00"$i
+  elif (($i<1000)); then
+    text="rods_0"$i
+  else
+    text="rods_"$i
+  fi
+  mv $f carpetaNueva/$text
+  let i=i+1
+done
