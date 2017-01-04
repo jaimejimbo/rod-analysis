2f027b8b43035e4c80e8bdd8e562d494f41b795d
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2017-01-04 16:24:53 +0100

    - Now it creates an animation. - It separates K6 and K12 rods. - It prints progress.

--- density_animation.py
+++ density_animation.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 import sqlite3 as sql
 from matplotlib import animation
 
-resol = 10
+
+_writer = animation.writers['ffmpeg']
+writer = _writer(fps=15, metadata=dict(artist='Jaime Perez Aparicio'), bitrate=1800)
+WRITER = writer
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+CLEAR_LAST = CURSOR_UP_ONE + ERASE_LINE # + CURSOR_UP_ONE
+
+resol = 60
 sigma = 1
 
 connection = sql.connect("rods.db")
@@ -19,18 +27,31 @@ experiment_ids = cursor.fetchall()
 experiment_ids = [experiment_id[0] for experiment_id in experiment_ids]
 
 
-for experiment_id in experiment_ids:
-    file_ids = cursor.execute(get_file_ids_sql, (experiment_id,))
-    for file_id in file_ids:
-        distr = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
-        cursor2.execute(get_rods_sql, (experiment_id, file_id[0]))
+def plot_frame(experiment_id_, file_ids):
+    """
+    Plots a frame doing some averages over burst.
+    """
+    distr = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+    for index in range(5):
+        distr_6 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+        distr_12 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+        cursor2.execute(get_rods_sql, (str(experiment_id_), str(file_ids[index])))
         rods = cursor2.fetchall()
+        #len(file_ids) can be < 5
+        if not len(rods):
+            print("Empty")
+            return
         rods = np.array([np.array(rod) for rod in rods])
+        rods_6, rods_12 = [], []
+        for rod in rods:
+            if 6 < float(rod[2])/rod[3] < 12:
+                rods_6.append(rod)
+            elif 12 < float(rod[2])/rod[3] < 20:
+                rods_12.append(rod)
+        rods_6, rods_12 = np.array(rods_6), np.array(rods_12)
         num_rods = len(rods)
-        xvals = rods[:,0]
-        yvals = rods[:,1]
-        x0 = min(xvals)
-        y0 = min(yvals)
+        xvals, yvals = rods[:, 0], rods[:, 1]
+        x0, y0 = min(xvals), min(yvals)
         dx = float(max(xvals)-x0)/resol
         dy = float(max(yvals)-y0)/resol
         rad = (dx*resol/2.0 + dy*resol/2.0)/2.0
@@ -39,17 +60,44 @@ for experiment_id in experiment_ids:
             for col in range(resol):
                 pos = np.array([col*dx+x0, row*dy+y0])
                 if np.sum((pos-center)**2) <= rad**2:
-                    val = -((xvals-col*dx-x0)**2/dx**2+np.abs(yvals-row*dy-y0)**2/dy**2)
-                    exps = np.exp(val/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
-                    distr[row,col] += np.sum(exps)/num_rods
-        x_vals = np.array(range(resol))
-        y_vals = np.array(range(resol))
-        mesh = np.array(np.meshgrid(x_vals, y_vals))
-        x_vals, y_vals = tuple(mesh.reshape(2,resol**2))
-        distr = list(distr.reshape(1,len(distr)**2)[0])
-        size = 400
-        plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
-        plt.show()
+                    val_6 = -((rods_6[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_6[:, 1]-row*dy-y0)**2/dy**2)
+                    exps_6 = np.exp(val_6/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
+                    distr_6[row, col] += np.sum(exps_6)/num_rods
+                    val_12 = -((rods_12[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_12[:, 1]-row*dy-y0)**2/dy**2)
+                    exps_12 = np.exp(val_12/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
+                    distr_12[row, col] += np.sum(exps_12)/num_rods
+        distr += (distr_12-distr_6)/(distr_12+distr_6)/5.0
+    x_vals, y_vals = np.array(range(resol)), np.array(range(resol))
+    mesh = np.array(np.meshgrid(x_vals, y_vals))
+    x_vals, y_vals = tuple(mesh.reshape(2, resol**2))
+    distr = list(distr.reshape(1, len(distr)**2)[0])
+    size = 4000.0/resol
+    plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
+    cb = plt.colorbar()
+    cb.set_label('(n_12-n_6)/(n_12+n_6)')
+    plt.xlabel("x [norm]")
+    plt.ylabel("y [norm]")
+    plt.suptitle('density distribution')
+
+for experiment_id in experiment_ids:
+    print(experiment_id)
+    file_ids = cursor.execute(get_file_ids_sql, (experiment_id,)).fetchall()
+    fig = plt.figure()
+    num_frames = int(len(file_ids)/5)
+    print("\n")
+    def animate(frame_idx):
+        """
+        Wrapper
+        """
+        progress = CLEAR_LAST + str(frame_idx*5) + "/" + str(num_frames) + "("
+        progress += str(frame_idx*500.0/num_frames) + "%)"
+        print(progress)
+        file_id0 = frame_idx*5
+        plot_frame(int(experiment_id), file_ids[file_id0:file_id0+5])
+    anim = animation.FuncAnimation(fig, animate, frames=num_frames)
+    name = 'density_animation{}.mp4'.format(experiment_id)
+    anim.save(name)
+    print(CLEAR_LAST)
 
 connection.commit()
 connection.close()
