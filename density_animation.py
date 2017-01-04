a61f3d2d65e2a6a01907241c94d211adafe14be2
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2017-01-04 21:01:44 +0100

    - Only update colors when animating, instead of ploting the whole frame. - Fix progress info (there was a 5* multiplier). - Fix sql query for getting file ids (repeated file ids wasn't filtered).

--- density_animation.py
+++ density_animation.py
@@ -4,6 +4,7 @@ import math
 import matplotlib.pyplot as plt
 import sqlite3 as sql
 from matplotlib import animation
+import matplotlib
 
 
 _writer = animation.writers['ffmpeg']
@@ -11,23 +12,26 @@ writer = _writer(fps=15, metadata=dict(artist='Jaime Perez Aparicio'), bitrate=1
 WRITER = writer
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
-CLEAR_LAST = CURSOR_UP_ONE + ERASE_LINE # + CURSOR_UP_ONE
+CLEAR_LAST = CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE
 
 resol = 60
+cresol = 1000
 sigma = 1
 
 connection = sql.connect("rods.db")
 cursor = connection.cursor()
 cursor2 = connection.cursor()
 get_rods_sql = "select xmid,ymid,major,minor from datos where experiment_id=? and file_id=?"
-get_file_ids_sql = "select file_id from datos where experiment_id=?"
+get_file_ids_sql = "select distinct file_id from datos where experiment_id=?"
 
 cursor.execute("select distinct experiment_id from datos")
 experiment_ids = cursor.fetchall()
 experiment_ids = [experiment_id[0] for experiment_id in experiment_ids]
 
+colors = plt.cm.jet(np.linspace(-1,1,cresol))
 
-def plot_frame(experiment_id_, file_ids):
+
+def _plot_frame(experiment_id_, file_ids, plot):
     """
     Plots a frame doing some averages over burst.
     """
@@ -66,38 +70,56 @@ def plot_frame(experiment_id_, file_ids):
                     val_12 = -((rods_12[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_12[:, 1]-row*dy-y0)**2/dy**2)
                     exps_12 = np.exp(val_12/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
                     distr_12[row, col] += np.sum(exps_12)/num_rods
+        #average over burst
         distr += (distr_12-distr_6)/(distr_12+distr_6)/5.0
-    x_vals, y_vals = np.array(range(resol)), np.array(range(resol))
-    mesh = np.array(np.meshgrid(x_vals, y_vals))
-    x_vals, y_vals = tuple(mesh.reshape(2, resol**2))
     distr = list(distr.reshape(1, len(distr)**2)[0])
-    size = 4000.0/resol
-    plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
-    cb = plt.colorbar()
-    cb.set_label('(n_12-n_6)/(n_12+n_6)')
-    plt.xlabel("x [norm]")
-    plt.ylabel("y [norm]")
-    plt.suptitle('density distribution')
+    if not(plot is None):
+        #only update colors.
+        colors_ = [colors[int(value*cresol/2)] for value in distr]
+        plot.set_color(colors)
+        return plot
+    else:
+        #first plot. It create axes...
+        x_vals, y_vals = np.array(range(resol)), np.array(range(resol))
+        mesh = np.array(np.meshgrid(x_vals, y_vals))
+        x_vals, y_vals = tuple(mesh.reshape(2, resol**2))
+        size = 4000.0/resol
+        plot = plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
+        cb = plt.colorbar()
+        cb.set_label('(n_12-n_6)/(n_12+n_6)')
+        plt.xlabel("x [norm]")
+        plt.ylabel("y [norm]")
+        plt.suptitle('density distribution')
+        first = False
+        return plot
 
-for experiment_id in experiment_ids:
+def create_animation(experiment_id):
+    """
+    Creates animation for an experiment.
+    """
     print(experiment_id)
     file_ids = cursor.execute(get_file_ids_sql, (experiment_id,)).fetchall()
     fig = plt.figure()
     num_frames = int(len(file_ids)/5)
     print("\n")
+    plot = None
     def animate(frame_idx):
         """
         Wrapper
         """
+        global plot
         progress = CLEAR_LAST + str(frame_idx*5) + "/" + str(num_frames) + "("
-        progress += str(frame_idx*500.0/num_frames) + "%)"
-        print(progress)
+        progress += "{0:.2f}%)"
+        print(progress.format(frame_idx*100.0/num_frames))
         file_id0 = frame_idx*5
-        plot_frame(int(experiment_id), file_ids[file_id0:file_id0+5])
+        plot = _plot_frame(int(experiment_id), file_ids[file_id0:file_id0+5] , plot)
     anim = animation.FuncAnimation(fig, animate, frames=num_frames)
     name = 'density_animation{}.mp4'.format(experiment_id)
     anim.save(name)
-    print(CLEAR_LAST)
+
+
+for experiment_id in experiment_ids:
+    create_animation(experiment_id)
 
 connection.commit()
 connection.close()
