69af4ec34d8da5bd3e5f7c4d807e53c844592f65
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2017-01-05 13:23:56 +0100

    Performance changes
    - Use Pool to make matrix computation multiprocessed.
    - Save video when KeyboardInterrupt.

--- density_animation.py
+++ density_animation.py
@@ -1,3 +1,7 @@
+"""
+Density animation creation module.
+"""
+
 import numpy as np
 import scipy as sp
 import math
@@ -5,7 +9,10 @@ import matplotlib.pyplot as plt
 import sqlite3 as sql
 from matplotlib import animation
 import matplotlib
+from multiprocessing import Pool
+import itertools
 
+np.warnings.filterwarnings('ignore')
 
 _writer = animation.writers['ffmpeg']
 writer = _writer(fps=15, metadata=dict(artist='Jaime Perez Aparicio'), bitrate=1800)
@@ -29,21 +36,41 @@ experiment_ids = cursor.fetchall()
 experiment_ids = [experiment_id[0] for experiment_id in experiment_ids]
 
 colors = plt.cm.jet(np.linspace(-1,1,cresol))
+plot_ = None
+fig = plt.figure()
 
 
-def _plot_frame(experiment_id_, file_ids, plot):
+def pool_func(row, col, rods_6, rods_12, dx, dy, x0, y0, center, rad, num_rods):
     """
-    Plots a frame doing some averages over burst.
+    Function to be used in pool.
+    """
+    pos = np.array([col*dx+x0, row*dy+y0])
+    if np.sum((pos-center)**2) <= rad**2:
+        distr_6 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+        distr_12 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+        val_6 = -((rods_6[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_6[:, 1]-row*dy-y0)**2/dy**2)
+        exps_6 = np.exp(val_6/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
+        distr_6[row, col] += np.sum(exps_6)/num_rods
+        val_12 = -((rods_12[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_12[:, 1]-row*dy-y0)**2/dy**2)
+        exps_12 = np.exp(val_12/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
+        distr_12[row, col] += np.sum(exps_12)/num_rods
+        #average over burst
+        return (distr_12-distr_6)/(distr_12+distr_6)/5.0
+    return np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
+    
+def _get_distr(experiment_id_, file_ids):
+    """
+    Computes plottable data.
     """
     distr = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
     for index in range(5):
-        distr_6 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
-        distr_12 = np.array([[0.0 for dummy1 in range(resol)] for dummy2 in range(resol)])
         cursor2.execute(get_rods_sql, (str(experiment_id_), str(file_ids[index])))
         rods = cursor2.fetchall()
         #len(file_ids) can be < 5
         if not len(rods):
             print("Empty")
+            print(experiment_id_)
+            print(file_ids)
             return
         rods = np.array([np.array(rod) for rod in rods])
         rods_6, rods_12 = [], []
@@ -60,63 +87,90 @@ def _plot_frame(experiment_id_, file_ids, plot):
         dy = float(max(yvals)-y0)/resol
         rad = (dx*resol/2.0 + dy*resol/2.0)/2.0
         center = np.array([(min(xvals)+max(xvals))/2.0, (min(yvals)+max(yvals))/2.0])
-        for row in range(resol):
-            for col in range(resol):
-                pos = np.array([col*dx+x0, row*dy+y0])
-                if np.sum((pos-center)**2) <= rad**2:
-                    val_6 = -((rods_6[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_6[:, 1]-row*dy-y0)**2/dy**2)
-                    exps_6 = np.exp(val_6/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
-                    distr_6[row, col] += np.sum(exps_6)/num_rods
-                    val_12 = -((rods_12[:, 0]-col*dx-x0)**2/dx**2+np.abs(rods_12[:, 1]-row*dy-y0)**2/dy**2)
-                    exps_12 = np.exp(val_12/(4*sigma**2))/np.sqrt(2*math.pi)*sigma
-                    distr_12[row, col] += np.sum(exps_12)/num_rods
-        #average over burst
-        distr += (distr_12-distr_6)/(distr_12+distr_6)/5.0
+        rows, cols = np.meshgrid(range(resol), range(resol))
+        rows = np.array(rows).reshape(1,resol**2)[0]
+        cols = np.array(cols).reshape(1,resol**2)[0]
+        pool = Pool(4)
+        r = itertools.repeat
+        izip = zip(rows, cols, r(rods_6),
+                   r(rods_12), r(dx), r(dy),
+                   r(x0), r(y0), r(center),
+                   r(rad), r(num_rods))
+        distr = sum(pool.starmap(pool_func, izip))
+    print(distr)
+#all values are nan
+    distr[np.argwhere(np.isnan(distr))] = -2
     distr = list(distr.reshape(1, len(distr)**2)[0])
-    if not(plot is None):
-        #only update colors.
-        colors_ = [colors[int(value*cresol/2)] for value in distr]
-        plot.set_color(colors)
-        return plot
-    else:
-        #first plot. It create axes...
-        x_vals, y_vals = np.array(range(resol)), np.array(range(resol))
-        mesh = np.array(np.meshgrid(x_vals, y_vals))
-        x_vals, y_vals = tuple(mesh.reshape(2, resol**2))
-        size = 4000.0/resol
-        plot = plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
-        cb = plt.colorbar()
-        cb.set_label('(n_12-n_6)/(n_12+n_6)')
-        plt.xlabel("x [norm]")
-        plt.ylabel("y [norm]")
-        plt.suptitle('density distribution')
-        first = False
-        return plot
+    return distr
+    
+
+def _plot_initial_frame(experiment_id_, file_ids, plot, fig):
+    """
+    Plots initial frame.
+    """
+    distr = _get_distr(experiment_id_, file_ids)
+    #first plot. It create axes...
+    x_vals, y_vals = np.array(range(resol)), np.array(range(resol))
+    mesh = np.array(np.meshgrid(x_vals, y_vals))
+    x_vals, y_vals = tuple(mesh.reshape(2, resol**2))
+    size = 2000.0/resol
+    plot = plt.scatter(x_vals, y_vals, c=distr, marker='s', s=size)
+    #cb = plt.colorbar()
+    #cb.set_label('(n_12-n_6)/(n_12+n_6)')
+    plt.xlabel("x [norm]")
+    plt.ylabel("y [norm]")
+    plt.suptitle('density distribution')
+    #fig.draw()
+    #plt.show()
+    return plot
+
+def _plot_frame(experiment_id_, file_ids, plot, fig):
+    """
+    Updates the frame.
+    """
+    #only update colors.
+    distr = _get_distr(experiment_id_, file_ids)
+    colors_ = [colors[int(value*cresol/2)] for value in distr]
+    plot.set_color(colors)
+    #fig.draw()
+    return plot
 
 def create_animation(experiment_id):
     """
     Creates animation for an experiment.
     """
-    print(experiment_id)
     file_ids = cursor.execute(get_file_ids_sql, (experiment_id,)).fetchall()
-    fig = plt.figure()
+    file_ids = [file_id[0] for file_id in file_ids]
     num_frames = int(len(file_ids)/5)
-    print("\n")
-    plot = None
+    global plot_
+    global fig
+    plot_ = None
+    exit = False
+    frame_idx = 0
+    file_id0 = frame_idx*5
+    plot_ = _plot_initial_frame(int(experiment_id), file_ids[file_id0:file_id0+5] , plot_, fig)
     def animate(frame_idx):
         """
         Wrapper
         """
-        global plot
-        progress = CLEAR_LAST + str(frame_idx*5) + "/" + str(num_frames) + "("
+        global plot_
+        global fig
+        progress = CLEAR_LAST + str(frame_idx) + "/" + str(num_frames) + "("
         progress += "{0:.2f}%)"
         print(progress.format(frame_idx*100.0/num_frames))
         file_id0 = frame_idx*5
-        plot = _plot_frame(int(experiment_id), file_ids[file_id0:file_id0+5] , plot)
-    anim = animation.FuncAnimation(fig, animate, frames=num_frames)
+        if plot_ is None:
+            plot_ = _plot_initial_frame(int(experiment_id), file_ids[file_id0:file_id0+5] , plot_, fig)
+        else:
+            plot_ = _plot_frame(int(experiment_id), file_ids[file_id0:file_id0+5] , plot_, fig)
+    try:
+        anim = animation.FuncAnimation(fig, animate, frames=num_frames, repeat=False)
+    except KeyboardInterrupt:
+        exit = True
     name = 'density_animation{}.mp4'.format(experiment_id)
     anim.save(name)
-
+    if exit:
+        raise KeyboardInterrupt
 
 for experiment_id in experiment_ids:
     create_animation(experiment_id)
